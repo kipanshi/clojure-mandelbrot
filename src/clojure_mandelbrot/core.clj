f725417731d889ec5161a5b04f649e83684a0af0
Kirill Panshin <kipanshi@gmail.com>
2012-12-24 23:18:53 +0700

    Add functions for writing PPM image

--- src/clojure_mandelbrot/core.clj
+++ src/clojure_mandelbrot/core.clj
@@ -1,6 +1,6 @@
 (ns clojure-mandelbrot.core
-  (:gen-class))
-
+  (:gen-class)
+  (:use clojure.java.io))
 
 ;; CONSTANTS
 (def image-height 1000)
@@ -13,6 +13,34 @@
 
 (def max-iterations 30)
 
+
+;; FUNCTIONS
+(defn write-pixel
+  "Writes the pixel to the writer. Input is tuple of 3 integers representing RGB"
+  [wrtr pixel-tuple]
+  (.write wrtr (apply format (cons "%d %d %d " pixel-tuple)))
+)
+
+(defn write-pixels
+  "Write a collection of pixels"
+  [wrtr pixel-seq]
+  (dorun (map (fn [pixel-tuple] (write-pixel wrtr pixel-tuple)) pixel-seq))
+)
+
+(defn write-header
+  "Write PPN header"
+  [wrtr width height color-depth]
+  (.write wrtr (format "P3\n%d %d\n%d\n" width height color-depth))
+)
+
+(defn write-image
+  "Writes image to the given path. Input is a collection of pixels"
+  [write-path width height color-depth pixel-seq]
+  (with-open [wrtr (writer write-path)]
+    (write-header wrtr width height color-depth)
+    (write-pixels wrtr pixel-seq)
+    )
+)
 
 (defn -main
   "Calculate Mandelbrot set and write image file."
--- test/clojure_mandelbrot/core_test.clj
+++ test/clojure_mandelbrot/core_test.clj
@@ -1,7 +1,43 @@
 (ns clojure-mandelbrot.core-test
   (:use clojure.test
-        clojure-mandelbrot.core))
+        clojure-mandelbrot.core
+        clojure.java.io))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def TEST-FILE "/tmp/clojure-mandelbrot.core-test.ppm")
+
+
+(deftest test-write-to-test-file
+  (testing "Writing to test file"
+    (with-open [wrtr (writer TEST-FILE)]
+      (.write wrtr "hello world"))
+    (is (= "hello world" (slurp TEST-FILE)))
+))
+
+(deftest test-write-pixel
+  (testing "Writing pixel to the opened writer"
+    (with-open [wrtr (writer TEST-FILE)]
+      (write-pixel wrtr '(0 65355 0)))
+    (is (= "0 65355 0 " (slurp TEST-FILE)))
+))
+
+(deftest test-write-pixels
+  (testing "Write sequence of pixels"
+    (with-open [wrtr (writer TEST-FILE)]
+      (write-pixels wrtr '((0 65355 0) (0 0 0))))
+    (is (= "0 65355 0 0 0 0 " (slurp TEST-FILE)))
+))
+
+(deftest test-write-header
+  (testing "Write PPM header to the opened writer"
+    (with-open [wrtr (writer TEST-FILE)]
+      (write-header wrtr 600 800 65355))
+    (is (= "P3\n600 800\n65355\n" (slurp TEST-FILE)))
+))
+
+(deftest test-write-image
+  (testing "Write PPM image to the given path"
+    (write-image TEST-FILE 2 3 65355
+      '((65355 0 0) (0 65355 0) (0 0 65355)
+              (65355 65355 0) (0 65355 65355) (65355 0 65355)))
+    (is (= "P3\n2 3\n65355\n65355 0 0 0 65355 0 0 0 65355 65355 65355 0 0 65355 65355 65355 0 65355 " (slurp TEST-FILE)))
+))
