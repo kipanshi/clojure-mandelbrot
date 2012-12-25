29693b8eea5581bfdfe3d1ecb2cb957500bc4b12
Kirill Panshin <kipanshi@gmail.com>
2012-12-25 16:32:37 +0700

    Mandelbrot generates finally
        - without antialiasing
        - by some reason it's squizzed

--- src/clojure_mandelbrot/core.clj
+++ src/clojure_mandelbrot/core.clj
@@ -3,16 +3,24 @@
   (:use clojure.java.io))
 
 ;; CONSTANTS
-(def image-height 1000)
+(def image-height 1500)
 (def image-width 1000)
+(def color-depth 65355)
 
 (def min-re -2.0)
 (def max-re 1.0)
 (def min-im -1.2)
 (def max-im (+ min-im (* (- max-re min-re) (/ image-height image-width))))
 
-(def max-iterations 30)
+(def re-factor (/ (- max-re min-re) (- image-width 1)))
+(def im-factor (/ (- max-im min-im) (- image-height 1)))
 
+(def max-iterations 100)
+
+(def background-pixel '(0 0 32000))
+(def foreground-pixel '(65355 65355 65355))
+
+(def OUTPUT-FILE "Mandelbrot.ppm")
 
 ;; FUNCTIONS
 (defn write-pixel
@@ -42,9 +50,41 @@
     )
 )
 
+(defn process-iteration
+  "Process an iteration for the pixel"
+  [n c-re c-im z-re z-im]
+  (let [z-re2 (* z-re z-re)
+        z-im2 (* z-im z-im)]
+    (if (< n max-iterations)
+      (if (< (+ z-re2 z-im2) 4)
+        (process-iteration (inc n) c-re c-im
+              (+ c-re (- z-re2 z-im2)) (+ (* 2 z-re z-im) c-im))
+        false)
+      true))
+)
+
+(defn process-pixel
+  "Process a pixel"
+  [wrtr x y c-im]
+  (let [c-re (+ min-re (* x re-factor))
+        z-re c-re
+        z-im c-im]
+    (if (true? (process-iteration 0 c-re c-im z-re z-im))
+      (write-pixel wrtr foreground-pixel) (write-pixel wrtr background-pixel))
+))
+
+(defn process-y
+  "Process each row"
+  [wrtr y]
+  (let [c-im (- max-im (* y im-factor))]
+    (dorun (map (fn [x] (process-pixel wrtr x y c-im)) (range 1 image-width))))
+)
+
 (defn -main
   "Calculate Mandelbrot set and write image file."
   [& args]
-  (println "Hello, World!")
-
+  (with-open [wrtr (writer OUTPUT-FILE)]
+    (write-header wrtr image-width image-height color-depth)
+    (dorun (map (fn [y] (process-y wrtr y)) (range 1 image-height)))
+    )
 )
